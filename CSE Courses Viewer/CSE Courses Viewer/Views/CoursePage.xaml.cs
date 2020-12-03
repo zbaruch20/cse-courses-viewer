4abc221b1d5bf49eb4b1b5de1943e2723fea22a3
Zach Baruch <zbaruch20@gmail.com>
2020-12-03 01:06:19 -0500

    Created CoursePage view

---
+++ CSE Courses Viewer/CSE Courses Viewer/Views/CoursePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CSE_Courses_Viewer
+{
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CoursePage : ContentPage
+    {
+        public CoursePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
